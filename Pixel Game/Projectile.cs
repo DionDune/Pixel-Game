4914b33e923e78cf858a3362cf731e02e0afbdc2
DionDune <tomas.reynolds935@gmail.com>
2023-06-12 14:08:19 +0100

    Constructed the "Projectile" class

---
+++ Pixel Game/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pixel_Game
+{
+    internal class Projectile
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public int Momentum_Vertical { get; set; }
+        public int Momentum_Horizontal { get; set; }
+
+        public Projectile()
+        {
+            x = 0;
+            y = 0;
+
+            Momentum_Vertical = 0;
+            Momentum_Horizontal = 0;
+        }
+    }
+}
