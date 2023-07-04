31d31d19b8be57c8049ee49c3bbc1e5a98dd3ac0
DionDune <tomas.reynolds935@gmail.com>
2023-07-04 15:46:26 +0100

    Revert "Simplified useage of FillIngameBlock function"
    This reverts commit 5bf6bdde3dbc282d9deb9ce016ed0277c6024fd6.

--- Pixel Game/Form1.cs
+++ Pixel Game/Form1.cs
@@ -2696,12 +2696,12 @@ namespace Pixel_Game
             };
         }
 
-        private void Drawing_FillIngameBlock(Graphics canvas, SolidBrush Colour, int StartX, int StartY)
+        private void Drawing_FillIngameBlock(Graphics canvas, SolidBrush Colour, int StartX, int StartY, int SizeX, int SizeY)
         {
             canvas.FillRectangle(Colour, new Rectangle(
                 StartX - cameraOffset_x,
                 StartY - cameraOffset_y,
-                blockWidth, blockHeight
+                SizeX, SizeY
                 ));
         }
 
